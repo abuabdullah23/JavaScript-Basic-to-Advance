20bb73c14a0340bf6ab92f9348b0006f7e81bf7c
abuabdullah23 <abuobaidazayed@gmail.com>
2024-01-03 11:57:10 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/** 
+// Data type
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job); // undefined
+
+job = 'Teacher';
+console.log(job); // Teacher
+
+
+// wrong declaration
+var 3years = 3; // before 3, should use '_' or '$' sign.
+var john/mark = 'Jhon and Mark'; // should be use other case, like camel case
+var if = 35; // if is a reserve keyword in javascript
+
+// correct declaration
+var _3years = 3;
+var johnMark = 'John and Mark';
+var $if = 35; 
+
+*/
+
